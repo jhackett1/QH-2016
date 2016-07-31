8970abf75d2af07a08dd62c06fa07e0006a5217d
jhackett1 <hello@joshuahackett.com>
2016-07-31 23:57:05 +0100

    styling

--- front-page.php
+++ front-page.php
@@ -5,6 +5,9 @@
 <?php get_template_part( "masthead" ); ?>
 
 <!-- A section displaying three featured posts -->
+
+<?php if(is_home() && !is_paged()) { ?>
+
 <section id="featured">
 
   <div id="grad"></div>
@@ -14,10 +17,15 @@
     <?php
     //Declares a counter variable, which will count only the first three posts
     $counter = 0;
+    //The query
+    $feat_query= new WP_Query( array(
+      'cat' => 179
+    ));
+
     //The loop
-    if ( have_posts() ){
-      while ( have_posts() && $counter<4 ){
-        the_post();
+    if ( $feat_query->have_posts() ){
+      while ( $feat_query->have_posts() && $counter<4 ){
+        $feat_query->the_post();
 
         $feat = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' );
 $feat = $feat[0];
@@ -30,7 +38,7 @@ $feat = $feat[0];
               <a class="cover" href="<?php the_permalink(); ?>"></a>
               <div class="img" style="background-image:url(<?php echo $feat; ?>) "></div>
               <div class="meta">
-                <h5><?php the_category( ", " ); ?> | By <?php the_author(); ?></h5>
+                <h5><?php the_category( ", " ); ?> &middot; By <?php the_author(); ?></h5>
                 <h3><?php the_title(); ?></h3>
                 <hr>
                 <p><?php the_excerpt(); ?></p>
@@ -44,7 +52,7 @@ $feat = $feat[0];
             <div class="trail">
               <a class="cover" href="<?php the_permalink(); ?>"></a>
               <img src="<?php echo $feat; ?>" />
-              <h5><?php the_category( ", " ); ?> | By <?php the_author(); ?></h5>
+              <h5><?php the_category( ", " ); ?> &middot; By <?php the_author(); ?></h5>
               <h3><?php the_title(); ?></h3>
               <hr>
               <p><?php the_excerpt(); ?></p>
@@ -69,12 +77,16 @@ $feat = $feat[0];
   </div>
 </section>
 
-<section id="latest">
+<?php }; ?>
+
+<section id="latest" style=" background-image: url(http://localhost/wp/wp-content/themes/QH-2016/img/light_wool.png)">
   <div class="container thinner">
 
-    <h2><i class="fa fa-circle"></i> Latest updates</h2>
-
-
+    <?php if(is_paged()){ ?>
+      <h2>More updates</h2>
+    <?php } else { ?>
+      <h2><i class="fa fa-circle"></i> Latest updates</h2>
+    <?php }; ?>
 
     <div class="postbox grid">
     <?php
@@ -93,11 +105,11 @@ $feat = $feat[0];
           ?>
             <div class="post wow animated fadeInUp" data-wow-duration="0.3s" data-wow-offset="0">
               <a class="cover" href="<?php the_permalink(); ?>"></a>
-                              <img src="<?php echo $feat; ?>" />
+                <img src="<?php echo $feat; ?>" />
               <div>
                 <div id="readmore">Read Story <i class="fa fa-caret-right"></i></div>
 
-                <h5><?php the_category( ", " ); ?> | By <?php the_author(); ?></h5>
+                <h5><?php the_category( ", " ); ?> &middot; By <?php the_author(); ?></h5>
                 <h3><?php the_title(); ?></h3>
                 <hr>
                 <p><?php the_excerpt(); ?></p>
