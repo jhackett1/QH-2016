f03dffb253ddbc320795642e6e216135cc7e6a30
jhackett1 <hello@joshuahackett.com>
2016-08-05 22:57:29 +0100

    Fine-tuning

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html class="frontend" <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
   <?php wp_head(); ?>
 </head>
-<body>
+<body class="frontend">
 
 <section id="overlay"></section>
 
